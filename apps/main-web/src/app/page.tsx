9343d0a6758bfa33a8fc65aa812988b49af7bcb8
김태우 <taewoo080401@gmail.com>
2025-09-22 14:58:09 +0900

    feat: env

--- apps/main-web/src/app/page.tsx
+++ apps/main-web/src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@repo/ui/button";
 import Image from "next/image";
 
 export default async function Home() {
-  const { data } = await apiClient.get("https://jsonplaceholder.typicode.com/todos/1").withCookie();
+  const { data } = await apiClient.get("/todos/1").withCookie();
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
