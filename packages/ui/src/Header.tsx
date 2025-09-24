f8363c213ba1cdff50ac15599c691e9c9f4581b5
김태우 <taewoo080401@gmail.com>
2025-09-24 10:34:32 +0900

    feat: common tailwind css

---
+++ packages/ui/src/Header.tsx
@@ -0,0 +1,12 @@
+"use client"
+import "./style.css"
+
+const Header = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4 text-center font-bold">
+      Header
+    </div>
+  )
+}
+
+export default Header
